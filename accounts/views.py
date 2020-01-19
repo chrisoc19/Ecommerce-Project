1c42d2623a19546b4fc68d8f8dad34fa8a67cc6e
chrisoc19 <chrisoconnor904@yahoo.com.au>
2020-01-19 11:16:37 +0000

    added login functionality

--- accounts/views.py
+++ accounts/views.py
@@ -2,6 +2,7 @@ from django.shortcuts import render, redirect, reverse
 from django.contrib import auth, messages
 from accounts.forms import UserLoginForm
 
+
 def index(request):
     """Return the index.html file"""
     return render(request,  'index.html')
@@ -16,5 +17,18 @@ def logout(request):
 
 def login(request):
     """Return a login page"""
-    login_form = UserLoginForm()
+    if request.method == "POST":
+        login_form = UserLoginForm(request.POST)
+
+        if login_form.is_valid():
+            user = auth.authenticate(username=request.POST['username'],
+                                     password=request.POST['password'])
+            if user:
+                auth.login(user=user, request=request)
+                messages.success(request, "You have successfully logged in!")
+            else:
+                login_form.add_error(None,
+                                     "Your username or password is incorrect")
+    else:
+        login_form = UserLoginForm()
     return render(request, 'login.html', {'login_form': login_form})
