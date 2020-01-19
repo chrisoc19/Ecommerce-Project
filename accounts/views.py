5ba6c0ce863a37ebee9d44db5b595666963c3b3d
chrisoc19 <chrisoconnor904@yahoo.com.au>
2020-01-19 12:13:33 +0000

    added registration functionality

--- accounts/views.py
+++ accounts/views.py
@@ -42,6 +42,25 @@ def login(request):
 
 def registration(request):
     """Render the registration page"""
-    registration_form = UserRegistrationForm()
+    if request.user.is_authenticated:
+        return redirect(reverse('index'))
+
+    if request.method == "POST":
+        registration_form = UserRegistrationForm(request.POST)
+
+        if registration_form.is_valid():
+            registration_form.save()
+
+            user = auth.authenticate(username=request.POST['username'],
+                                     password=request.POST['password1'])
+            if user:
+                auth.login(user=user, request=request)
+                messages.success(request, "You have successfully registered")
+                return redirect(reverse('index'))
+            else:
+                messages.error(request,
+                               "Unable to register your account at this time")
+    else:
+        registration_form = UserRegistrationForm()
     return render(request, 'registration.html', {
         "registration_form": registration_form})
