aa3e5d362a3fb23a49d2c0176321a92557474ec5
chrisoc19 <chrisoconnor904@yahoo.com.au>
2020-01-19 17:00:42 +0000

    added bootstrap styling to forms and custom css

--- accounts/views.py
+++ accounts/views.py
@@ -5,40 +5,38 @@ from django.contrib.auth.models import User
 from accounts.forms import UserLoginForm, UserRegistrationForm
 
 
+# Create your views here.
 def index(request):
-    """Return the index.html file"""
-    return render(request,  'index.html')
+    """Return the index.html """
+    return render(request, 'index.html')
 
 
 @login_required
 def logout(request):
-    """Log the user out"""
+    """ Logs user out"""
     auth.logout(request)
-    messages.success(request, "You have successfully been logged out")
+    messages.success(request, "You have been successfully logged out!")
     return redirect(reverse('index'))
 
 
 def login(request):
-    """Return a login page"""
+    """ Retun a login page"""
     if request.user.is_authenticated:
         return redirect(reverse('index'))
     if request.method == "POST":
         login_form = UserLoginForm(request.POST)
-
         if login_form.is_valid():
             user = auth.authenticate(username=request.POST['username'],
                                      password=request.POST['password'])
-            messages.success(request, "You have successfully logged in!")
-
             if user:
                 auth.login(user=user, request=request)
+                messages.success(request, "You have successfully logged in!")
                 return redirect(reverse('index'))
             else:
-                login_form.add_error(None,
-                                     "Your username or password is incorrect")
+                login_form.add_error(None, "Your Username/Password is wrong!")
     else:
         login_form = UserLoginForm()
-    return render(request, 'login.html', {'login_form': login_form})
+    return render(request, 'login.html', {"login_form": login_form})
 
 
 def registration(request):
@@ -57,7 +55,6 @@ def registration(request):
             if user:
                 auth.login(user=user, request=request)
                 messages.success(request, "You have successfully registered")
-                return redirect(reverse('index'))
             else:
                 messages.error(request,
                                "Unable to register your account at this time")
