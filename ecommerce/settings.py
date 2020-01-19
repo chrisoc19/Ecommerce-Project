b85ec2beb29b6b347cdc4e2c97a9d8f6d0bbfd50
chrisoc19 <chrisoconnor904@yahoo.com.au>
2020-01-19 15:57:30 +0000

    Users can now log in with email adress instead username

--- ecommerce/settings.py
+++ ecommerce/settings.py
@@ -100,6 +100,10 @@ AUTH_PASSWORD_VALIDATORS = [
     },
 ]
 
+AUTHENTICATION_BACKENDS = [
+    'django.contrib.auth.backends.ModelBackend',
+    'accounts.backends.EmailAuth'
+]
 
 # Internationalization
 # https://docs.djangoproject.com/en/1.11/topics/i18n/
