f72049ff4b84379e98d431dd50ec84def4ae6c91
chrisoc19 <chrisoconnor904@yahoo.com.au>
2020-01-26 13:36:15 +0000

    can now view the categories page

--- products/urls.py
+++ products/urls.py
@@ -1,7 +1,8 @@
 from django.conf.urls import url, include
-from .views import all_products
+from .views import all_products, all_categories
 
 
 urlpatterns = [
     url(r'^$', all_products, name='products'),
+    url(r'^category$', all_categories, name='category'),
 ]
--- products/views.py
+++ products/views.py
@@ -1,8 +1,12 @@
 from django.shortcuts import render
-from .models import Product
+from .models import Product, Category
 
 
 # Create your views here.
 def all_products(request):
     products = Product.objects.all()
     return render(request, "products.html", {"products": products})
+
+
+def all_categories(request):
+    return render(request, "categories.html")
