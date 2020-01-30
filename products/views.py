42e7621b5f0e7dccbcb7daf97da7a6e9c7237a3d
chrisoc19 <chrisoconnor904@yahoo.com.au>
2020-01-30 14:11:23 +0000

    branch switch

--- products/views.py
+++ products/views.py
@@ -16,7 +16,7 @@ def product_home(request):
 
 
 def products_detail(request, product_slug):
-    products = Product.objects.get(slug=product_slug)
+    products = get_object_or_404(Product, slug=product_slug)
     return render(request, products_detail.html, {"products": products})
 
 
