2ae53dc09baccc925ceba56fffde96d2f566319a
chrisoc19 <chrisoconnor904@yahoo.com.au>
2020-02-13 10:55:07 +0000

    changed database settings to allow test to run

--- ecommerce/settings.py
+++ ecommerce/settings.py
@@ -83,18 +83,25 @@ WSGI_APPLICATION = 'ecommerce.wsgi.application'
 # https://docs.djangoproject.com/en/1.11/ref/settings/#databases
 
 if "DATABASE_URL" in os.environ:
-    DATABASES = {'default': dj_database_url.parse(os.environ.get('DATABASE_URL'))}
+    DATABASES = {'default': dj_database_url.parse(os.environ.get('DATABASE_URL')),
+    }
 else:
     print("Database URL not found. Using SQLite instead")
     DATABASES = {
         'default': {
             'ENGINE': 'django.db.backends.sqlite3',
             'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
-            'USER': 'chris',
-            'PASSWORD': 'mydb123',
+            'USER': 'django',                   
+            'PASSWORD': 'mydb123', 
+            
         }
     }
-
+# DATABASES = {
+#     'default': {
+#         'ENGINE': 'django.db.backends.sqlite3',
+#         'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
+#     }
+# }
 
 # Password validation
 # https://docs.djangoproject.com/en/1.11/ref/settings/#auth-password-validators
