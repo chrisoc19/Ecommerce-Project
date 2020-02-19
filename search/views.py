e824c502f6cebc63b08b025b5aadd8967810c0ae
chrisoc19 <chrisoconnor904@yahoo.com.au>
2020-02-19 21:25:43 +0000

    changed checkout form layout and minor touch ups

--- search/views.py
+++ search/views.py
@@ -1,26 +1,12 @@
 from django.shortcuts import render
 from products.models import Product
-from django.core.paginator import Paginator, EmptyPage, PageNotAnInteger
+from django.core.paginator import Paginator
 
 
 # Create your views here.
 def do_search(request):
     products = Product.objects.filter(name__icontains=request.GET['q'])
-    paginator = Paginator(products, 6)
-    page = request.GET.get('page')
-
-    try:
-        items = paginator.page(page)
-    except PageNotAnInteger:
-        items = paginator.page(1)
-    except EmptyPage:
-        items = paginator.page(paginator.num_pages)
-
-    index = items.number - 1
-    max_index = len(paginator.page_range)
-    start_index = index - 5 if index >= 5 else 0
-    end_index = + 5 if index <= max_index - 5 else max_index
-    page_range = paginator.page_range[start_index:end_index]
+    paginator = Paginator(products, 100)
+    items = paginator.page(paginator.num_pages)
     return render(request, "product_home.html", {"products": products,
-                                                 "items": items,
-                                                 "page_range": page_range, })
+                                                 "items": items})
