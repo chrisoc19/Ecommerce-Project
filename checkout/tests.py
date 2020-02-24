d394734b299c9c6f4dce0bfbfe0aa800b595633e
chrisoc19 <chrisoconnor904@yahoo.com.au>
2020-02-24 15:31:13 +0000

    added url test

--- checkout/tests.py
+++ checkout/tests.py
@@ -1,3 +1,16 @@
-from django.test import TestCase
+from django.test import TestCase, Client
+from django.urls import reverse, resolve
+from .views import checkout
+
 
 # Create your tests here.
+class Test_urls(TestCase):
+    """
+    Here we'll define the tests that we'll run against our
+    Product Urls
+    """
+    def test_checkout_url_is_resolved(self):
+        url = reverse('checkout')
+        self.assertEquals(resolve(url).func, checkout)
+
+
